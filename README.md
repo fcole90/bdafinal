5224723d970dbf310e58176a2570980e239c87f2
Fabio Colella <fcole90@gmail.com>
2017-11-30 10:09:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# BDA final
+
+Analysis of causes of death for viral hepatitis and sequelae of viral hepatitis.
+
+Dataset from: [Eurostat](http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=hlth_cd_anr&lang=en)
+
+Legal Notice on using data: [link](https://ec.europa.eu/info/legal-notice_en)
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+setup(
+    name="bdafinal",
+    packages=["bdafinal"],
+    version="0.0.1.dev1",
+    description="Bayesian Data Analysis - Final Project",
+    author="Fabio Colella, Michele Vantini",
+    author_email="fabio.colella@aalto.fi, michele.vantini@aalto.fi",
+    url="https://github.com/fcole90/bdafinal",
+    download_url="https://github.com/fcole90/bdafinal",
+    keywords=["bayesian", "data", "analysis", "death", "diseases"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research"
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+        ],
+    long_description="""\
+Algorithmic Methods of Data Mining - Final Project
+""",
+    install_requires=['matplotlib',
+                      'pystan',
+                      'numpy'],
+    python_requires='>=3.4'
+)
